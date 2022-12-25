27bc51f7beb2ead7ec02b58f5efeafd035bf500e
rhazari <raihan.hazarika@gmail.com>
2022-12-25 11:55:14 -0800

    Deposit example

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -18,20 +18,30 @@
 */
 
 TEST_CASE("Bank Account app getAmount()") {
-  std::vector<double> accounts = {1001, 2002, 3002, 4001};
-  BankApp bankApp(accounts);
+	std::vector<double> accounts = {1001, 2002, 3002, 4001};
+	BankApp bankApp(accounts);
 
-  REQUIRE(accounts.size() == 4);
+	REQUIRE(accounts.size() == 4);
 
-  SECTION("Negative index should throw") {
-    CHECK_THROWS(bankApp.getAmount(-1));
-  }
+	SECTION("Negative index should throw") {
+		CHECK_THROWS(bankApp.getAmount(-1));
+	}
 
-  SECTION("Index greater than account size should throw") {
-    CHECK_THROWS(bankApp.getAmount(4));
-  }
+	SECTION("Index greater than account size should throw") {
+		CHECK_THROWS(bankApp.getAmount(4));
+	}
 
-  SECTION("Should fetch the amount for account id 1") {
-    REQUIRE(bankApp.getAmount(1) == 2002);
-  }
+	SECTION("Should fetch the amount for account id 1") {
+		REQUIRE(bankApp.getAmount(1) == 2002);
+	}
+}
+
+TEST_CASE("Bank Account app depositAmount()") {
+    std::vector<double> accounts = {1001, 2002, 3002, 4001};
+    BankApp bankApp(accounts);
+
+	SECTION("Should fetch the amount for account id 1") {
+		CHECK_NOTHROW(bankApp.depositAmount(200, 1));
+		REQUIRE(bankApp.getAmount(1) == 2202);
+	}
 }
